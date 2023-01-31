3b7b5f45d240dd9591a398ffc4f079121004c2b7
Ignacio Furió José <bichoifj@gmail.com>
2023-01-31 16:14:25 +0100

    JS basics

---
+++ js/main.js
@@ -0,0 +1,64 @@
+// let variable1 = "";
+// let variable2;
+// let variable3 = null;
+
+// // console.log sirve para checkear el estado de las variables 
+
+// console.log(variable1)
+// console.log(variable2)
+// console.log(variable3)
+
+// // typeof indica que tipo de datos contiene una variable 
+
+// console.log(typeof(variable1))
+// console.log(typeof(variable2))
+// console.log(typeof(variable3))
+
+
+// let variable4 = true;
+// let variable5 = false;
+
+// console.log(variable4, variable5)
+
+// variable4 = false;
+// variable5 = true;
+
+// console.log(variable4, variable5)
+
+// Pruebas entre string y num 
+
+// let numero1 = 7;
+
+// console.log(-numero1);
+
+// console.log(numero1);
+
+// let numeroFeo = "12";
+// let numeroBonito = "200";
+
+// console.log(numeroFeo);
+
+// console.log(-numeroFeo);
+
+// console.log(numeroFeo + " " + numeroBonito)
+// // console.log(numeroBonito - numeroFeo);
+// // console.log(parseInt(numeroFeo) + parseInt(numeroBonito))
+
+// // ejemplo de If
+
+// let nombre = prompt("Introduce nombre");
+
+// if(nombre === ""){
+//     console.log("Nos me has dicho anda")
+// }else if(nombre === "Ignacio"){
+//     console.log("Gracias por venir")
+// }else{
+//     console.log("Bienvenido, ", nombre)
+// }
+
+
+
+
+
+
+
